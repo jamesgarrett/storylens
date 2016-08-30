5d6602d31a6c2fdaf4352726d55d5bada27fc521
jamesgarrett <jgsibinga@gmail.com>
2016-08-30 09:39:21 -0400

    Modular CSS
    Additions of modules, updates to archive

--- archive.php
+++ archive.php
@@ -12,18 +12,9 @@ get_header();
 		<div class="row">
 
 		 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		 <?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); ?>
 
 		 	<div class="col-md-6">
-		        <?php if (has_post_thumbnail( $post->ID ) ): ?>
-		        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-		        <div class="panel-upper"  <?php if ( has_post_thumbnail() ): ?>style="height:350px;background-size:cover;background-position:center;background-image:url('<? echo $large_image_url[0] ?>');"<?php endif; ?>>
-		        </div>
-		        <?php endif; ?>
-		        <div class="panel-lower">
-		            <h2><a href="<? the_permalink(); ?>"><? the_title(); ?></a></h2>
-		            <p><?php the_excerpt(); ?></p>
-		        </div>
+		        <?php get_the_title(); ?>
 		    </div>
 		    
 			<?php endwhile; else: ?>
